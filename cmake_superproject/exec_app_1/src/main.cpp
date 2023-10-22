41544bdcb4d372f9ba7dd07229cc085dbe4bba98
Oleg Donchenko <efrdoo@gmail.com>
2023-10-23 00:56:07 +0300

    Start implement superproject

---
+++ cmake_superproject/exec_app_1/src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "version.h"
+#include "exec_app_1.h"
+#include "subdir/subfile.h"
+
+int main (int, char **) {
+    std::cout << "Version: " << PROJECT_VERSION_PATCH << std::endl;
+    std::cout << THIS_NAME << std::endl;
+    std::cout << THIS_SUBFILE << std::endl;
+    return 0;
+}
